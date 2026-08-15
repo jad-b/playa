550e00231ab5c3e1372364789a79b56a2603201e
jdb <jeremydb@protonmail.com>
2026-08-15 07:13:50 -0400

    Additional programs

--- nixos/home.nix
+++ nixos/home.nix
@@ -18,20 +18,31 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
+    # apps
+    obsidian
+    spotify-player
+
+    # programming
+    cargo
+    neovim
+
+    # shell utilities
+    fzf
+    gnupg
+    jq
+    ripgrep
     tmux
+    tree
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+    # system monitoring
+    btop
+    dnsutils
+    iftop
+    iotop
+    nmap
+    pciutils
+    strace
+    usbutils
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
@@ -70,5 +81,5 @@
   };
 
   # Let Home Manager install and manage itself.
-}
   programs.home-manager.enable = true;
+}
